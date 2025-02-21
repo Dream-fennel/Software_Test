1139648f556f4befeb4043cfcb0d479548cc3901
Software_Test <942694173@qq.com>
2025-02-21 10:06:19 +0800

    111

---
+++ SQL文件/2.20.sql
@@ -0,0 +1,19 @@
+/*
+多行注释
+
+*/
+#单行注释
+
+
+SELECT 123,666;
+SELECT '好好学习吧';
+SELECT 1111;
+SELECT NAME AS 姓名 ,sex AS 性别
+FROM students;
+SELECT *
+FROM students;
+
+SELECT DISTINCT age
+FROM students;
+SELECT age
+FROM students;
